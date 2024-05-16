7f98e2ceca3d1b209b0eabacc21e364a995b5078
s23004 <s23004@std.it-college.ac.jp>
2024-05-16 09:28:08 +0900

    18章終了

---
+++ src/main/kotlin/Chap18.kt
@@ -0,0 +1,50 @@
+package org.example
+
+import kotlin.time.measureTime
+
+fun main(args: Array<String>) {
+    run {
+        println("----18.1----")
+        val s: Set<String> = setOf("あ", "い", "う", "え", "お")
+        println(s)
+    }
+    run {
+        println("----18.2----")
+        val s: MutableSet<String> = mutableSetOf("あ", "い", "う", "え", "お")
+        s += "か"
+        s += "き"
+        s += "く"
+        s += "け"
+        s += "こ"
+        println(s)
+    }
+    run {
+        println("----18.3----")
+        val s: MutableSet<String> = mutableSetOf("あ", "い", "う", "え", "お")
+        s += "か"
+        s += "き"
+        s += "く"
+        s += "い"
+        s += "お"
+        s += "か"
+        println(s)
+    }
+    val charList = 'a'..'z'
+    val searchCharList = mutableListOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+    val searchCharset = mutableSetOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+    val count = 1_000_000_000L
+    val listTime = measureTime {
+        for (i in 0..count) {
+            val target = charList.shuffled()[0]
+            println("「${target}」は存在する？ : ${target in searchCharList}")
+        }
+    }
+    val setTime = measureTime {
+        for (i in 0..count) {
+            val target = charList.shuffled()[0]
+            println("「${target}」は存在する？ : ${target in searchCharset}")
+        }
+    }
+    println("Listでかかった時間: ${listTime}")
+    println(" setでかかった時間: ${setTime}")
+}
